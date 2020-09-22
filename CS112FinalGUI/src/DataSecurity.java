609f9ee03b0f9259fd8a238043a1c9f3209b857e
aliciamstanton <54298148+aliciamstanton@users.noreply.github.com>
2020-09-21 17:56:39 -0700

    Delete DataSecurity.java

--- CS112FinalGUI/src/DataSecurity.java
+++
@@ -1,9 +0,0 @@
-
-public interface DataSecurity 
-{
-  // Method body that will execute the encryption
-  public Card Encrypt();
-
-// Method body that will execute the decryption
-  public Card Decrypt();
-}
