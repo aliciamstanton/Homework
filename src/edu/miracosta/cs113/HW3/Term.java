c2338dcd0bbc44ca969d56f93ecad75ab3d6cfaf
Alicia Stanton <atkelly013113@gmail.com>
2020-10-05 14:32:29 -0700

    Update Term.java

--- src/edu/miracosta/cs113/HW3/Term.java
+++ src/edu/miracosta/cs113/HW3/Term.java
@@ -1,6 +1,85 @@
+// Package declaration under HW3
+
 package edu.miracosta.cs113.HW3;
 
-public class Term 
+// Definition of Class Term which implements the Comparable Interface 
+public  class Term 
 {
+	
+	// Private Member variables
+	
+	// Exponent
+	private int exponent;
+	
+	// Coefficient
+	private int coefficient;
+	
+	
+	// Default Constructor
+	Term()
+	{
+		this.exponent = 1;
+		this.coefficient = 1;
+	}
+	
+	// Full Constructor
+	Term(int exponent, int coefficient)
+	{
+		this.exponent = exponent;
+		this.coefficient = coefficient;
+	}
+	
+	// Copy Constructor
+	
+	// Getter - Exponent
+	int getExponent()
+	{
+		return this.exponent;
+	}
+	
+	// Getter - Coefficient
+	int getCoefficient()
+	{
+		return this.coefficient;
+	}
+	
+	// Setter - Exponent
+	void setExponent(int exponent)
+	{
+		this.exponent = exponent;
+	}
+	
+	// Setter - Coefficient
+	void setCoefficient(int coefficient)
+	{
+		this.coefficient = coefficient;
+	}
+	
+	// Equals
+	
+	// ToString 
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+
+	/*@Override
+	public int compareTo(Object o) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+	
+	 */
+	
 
 }
