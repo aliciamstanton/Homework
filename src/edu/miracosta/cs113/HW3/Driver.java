22660bc6aa75ddc4c897a91e671ae724ddc1995c
Alicia Stanton <atkelly013113@gmail.com>
2020-10-05 14:32:08 -0700

    Making updates to Term and testing in Driver

--- src/edu/miracosta/cs113/HW3/Driver.java
+++ src/edu/miracosta/cs113/HW3/Driver.java
@@ -7,6 +7,21 @@ public class Driver
 	{
 		
 			System.out.println("Beginning of the Polynomial Program");
+			
+			// Test the Term class using the default 
+			Term polyTerm = new Term();
+			
+			System.out.println(polyTerm.getExponent());
+			System.out.println(polyTerm.getCoefficient());
+			
+			// Test the Term class using the Full Constructor
+			Term polyTerm2 = new Term(2,3);
+			
+			System.out.println(polyTerm2.getExponent());
+			System.out.println(polyTerm2.getCoefficient());
+			
+			
+			
 
 	}
 
