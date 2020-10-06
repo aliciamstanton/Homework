d085ecf57048bac80b314c782504ab5977dc84d7
Alicia Stanton <atkelly013113@gmail.com>
2020-10-06 12:28:46 -0700

    Adding Stubs based on PolynomialTest.java

--- src/edu/miracosta/cs113/HW3/Polynomial.java
+++ src/edu/miracosta/cs113/HW3/Polynomial.java
@@ -1,6 +1,76 @@
+
+// Package declaration for the Polynomial Class 
 package edu.miracosta.cs113.HW3;
 
+
+// Class Definition of Polynomial Class 
 public class Polynomial 
 {
+	
+	// Default Constructor
+	Polynomial()
+	{
+		
+	}
+	
+	// Copy Constructor
+	Polynomial(Polynomial object)
+	{
+		
+	}
+	
+	// Getter for Number of Terms
+	int getNumTerms()
+	{
+		return 0;
+		
+	};
+	
+	// Getter for a Specific term
+	Term getTerm(int index)
+	{
+		return Term;
+	}
+	
+	// Replace all items into a string format 
+	void replaceAll(String thing1, String thing2)
+	{
+		
+	}
+	
+	// Add a Term
+	void addTerm(Term termT)
+	{
+		
+	}
+	
+	// Add Entire Polynomials
+	void add(Polynomial p)
+	{
+		
+	}
+	
+	// Clear method
+	void clear()
+	{
+		
+	}
+	
+	// Equals Method
+	@Override
+	bool equals(Object obj)
+	{
+		return false;
+	}
+	
+	// To String
+	@Override
+	void toString()
+	{
+		
+	}
+	
+	
+	
 
 }
