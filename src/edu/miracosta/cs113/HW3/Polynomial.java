40e7ddf7ea3092ab9b771dc0a61bcaaef19a3dd8
Alicia Stanton <atkelly013113@gmail.com>
2020-10-07 09:45:41 -0700

    Add Access Modifier in Polynomial Class

--- src/edu/miracosta/cs113/HW3/Polynomial.java
+++ src/edu/miracosta/cs113/HW3/Polynomial.java
@@ -8,64 +8,64 @@ public class Polynomial
 {
 	
 	// Default Constructor
-	Polynomial()
+	public Polynomial()
 	{
 		
 	}
 	
 	// Copy Constructor
-	Polynomial(Polynomial object)
+	public Polynomial(Polynomial object)
 	{
 		
 	}
 	
 	// Getter for Number of Terms
-	int getNumTerms()
+	public int getNumTerms()
 	{
 		return 0;
 		
 	};
 	
 	// Getter for a Specific term
-	Term getTerm(int index)
+	public Term getTerm(int index)
 	{
 		return Term;
 	}
 	
 	// Replace all items into a string format 
-	void replaceAll(String thing1, String thing2)
+	public void replaceAll(String thing1, String thing2)
 	{
 		
 	}
 	
 	// Add a Term
-	void addTerm(Term termT)
+	public void addTerm(Term termT)
 	{
 		
 	}
 	
 	// Add Entire Polynomials
-	void add(Polynomial p)
+	public void add(Polynomial p)
 	{
 		
 	}
 	
 	// Clear method
-	void clear()
+	public void clear()
 	{
 		
 	}
 	
 	// Equals Method
 	@Override
-	bool equals(Object obj)
+	public bool equals(Object obj)
 	{
 		return false;
 	}
 	
 	// To String
 	@Override
-	void toString()
+	public String toString()
 	{
 		
 	}
