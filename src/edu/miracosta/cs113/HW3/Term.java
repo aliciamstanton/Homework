db8a9705af3c3345208176ba8434a037789f9427
Alicia Stanton <atkelly013113@gmail.com>
2020-10-09 09:49:14 -0700

    Updating from toString to ToString

--- src/edu/miracosta/cs113/HW3/Term.java
+++ src/edu/miracosta/cs113/HW3/Term.java
@@ -207,7 +207,7 @@ public class Term implements Comparable, Cloneable
 	}
 	
 	// ToString 
-	public String toString()
+	public String ToString()
 	{
 		// Create an empty string to hold the term we will return
 		String fullTerm = "";
