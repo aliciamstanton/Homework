142324444c3e530f575743d440c4a745c9383385
Alicia Stanton <atkelly013113@gmail.com>
2020-10-10 08:44:50 -0700

    Creating switch statement in Driver

--- src/edu/miracosta/cs113/HW3/Driver.java
+++ src/edu/miracosta/cs113/HW3/Driver.java
@@ -1,5 +1,7 @@
 package edu.miracosta.cs113.HW3;
 
+import java.util.Scanner;
+
 public class Driver 
 {
 
@@ -18,7 +20,74 @@ public class Driver
 		System.out.println("7. Add Polynomial A to Polynomial B");
 		System.out.println("8. Exit Program");
 		
+		Scanner input = new Scanner(System.in);
 		
+		
+		// Create a string response to hold the user response
+		String userChoice = input.next();
+		
+		// Create a switch statement that will run each item in the list
+		switch(userChoice)
+		{
+		
+				// Case 1 - Create Poly A 
+				case "1":
+				{
+					
+					break;
+			
+				}
+				
+				// Case 2 - Create Poly B 
+				case "2":
+				{
+					
+					break;
+				}
+				
+				// Case 3 - Add Term Poly A
+				case "3":
+				{
+					
+					break;
+				}
+				
+				// Case 4 - Add Term Poly B
+				case "4":
+				{
+					break;
+				}
+				
+				// Case 5 - Clear Poly A
+				case "5":
+				{
+					break;
+				}
+				
+				// Case 6 - Clear Poly B
+				case "6":
+				{
+					break;
+				}
+				
+				// Case 7 - Add Poly A and Poly B 
+				case "7":
+				{
+					break;
+				}
+		
+				// Case 8 - Exit the program 
+				case "8":
+				{
+					break;
+				}
+		
+				default:
+				{
+					System.out.println("None of the choices were valid.");
+					break;
+				}
+		}
 		
 		
 		
