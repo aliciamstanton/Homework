af8d5b6de770945cce588875347c02b232d5b60f
Alicia Stanton <atkelly013113@gmail.com>
2020-10-10 08:34:29 -0700

    Updating Driver Program

--- src/edu/miracosta/cs113/HW3/Driver.java
+++ src/edu/miracosta/cs113/HW3/Driver.java
@@ -6,10 +6,22 @@ public class Driver
 	public static void main(String[] args) 
 	{
 		
-			// Create the first and second polynomial
-		Polynomial A = new Polynomial();
+		// Create a Menu to show the user
+		System.out.println("Welcome to the Polynomial Creater");
+		System.out.println("Please choose an option below: ");
+		System.out.println("1. Create Polynomial A");
+		System.out.println("2. Create Polynomial B");
+		System.out.println("3. Add Term to Polynomial A");
+		System.out.println("4. Add Term to Polynomial B");
+		System.out.println("5. Clear Polynomial A");
+		System.out.println("6. Clear Polynomial B");
+		System.out.println("7. Add Polynomial A to Polynomial B");
+		System.out.println("8. Exit Program");
 		
-		Polynomial B = new Polynomial();
+		
+		
+		
+		
 		
 		
 			
