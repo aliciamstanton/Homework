f6edd86e8787bed7c1ed6e80c3c876f8ee294998
Alicia Stanton <atkelly013113@gmail.com>
2020-10-12 10:43:43 -0700

    Adding method signatures for required methods

--- src/edu/miracosta/cs113/HW4/DoubleLinkedList.java
+++ src/edu/miracosta/cs113/HW4/DoubleLinkedList.java
@@ -2,6 +2,7 @@
 // Homework 4 can be found in the HW4 Package 
 package edu.miracosta.cs113.HW4;
 
+import java.util.Iterator;
 import java.util.List;
 import java.util.ListIterator;
 
@@ -27,22 +28,122 @@ public class DoubleLinkedList<E> implements List<E>
 	
 	// Overridden and Required methods for List Interface 
 	
+	// Returns an iterator over the elements in the list 
+	@Override
+	public Iterator<E> iterator()
+	{
+		
+	}
+	
+	// Returns a list iterator over the elements in the list
+	@Override
+	public ListIterator<E> listIterator()
+	{
+		
+	}
+	
+	// Returns a list iterator over the elemnts in the list starting at specified position
+	@Override
+	public listIterator<E> listIterator(int index)
+	{
+		
+	}
+	
+	// Appends a specific element to the end of the list
+	@Override
+	public boolean add(E e)
+	{
+		
+	}
+	
+	// Inserts the specified element at the position in the list
+	@Override
+	public void add(int index, E element)
+	{
+		
+	}
+	
+	// Clears the linked list
+	@Override
+	public void clear()
+	{
+		
+	}
 	
 	
+	// Compares objects
+	@Override 
+	public boolean equals(Object o)
+	{
+		
+	}
 	
 	
+	// Returns true if the list contains a specific object
+	@Override
+	public boolean contains(Object o)
+	{
+		
+	}
 	
 	
+	// Get method for index
+	@Override
+	public E get(int index)
+	{
+		
+	}
 	
+	// Gets the index of a specific element
+	@Override
+	public int indexOf(Object o)
+	{
+		
+	}
 	
+	// Returns the index of the last occurrence of a specific object
+	@Override 
+	public int lastIndexOf(Object o)
+	{
+		
+	}
 	
+	// Returns true if the list is empty
+	@Override
+	public boolean isEmpty()
+	{
+		
+	}
 	
+	// Removes an element at a specific index
+	@Override
+	public E remove(int index)
+	{
+		
+	}
 	
+	// Removes the first occurrence of a specific element from the list
+	@Override
+	public boolean remove(Object o)
+	{
+		
+	}
 	
+	// Sets an element at a specified position
+	@Override
+	public E set(int index, E element)
+	{
+		
+	}
 	
+	// Returns the size of the list
+	@Override 
+	public int size()
+	{
+		
+	}
 	
-	
-	
+
 	
 	// Inner class that implements the ListIterator Interface
 	// Specific methods need to be implemented in order to use the interface 
