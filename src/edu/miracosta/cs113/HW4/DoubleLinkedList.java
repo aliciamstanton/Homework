778400aa008be6726fc51c65f5ff79466d2236bf
Alicia Stanton <atkelly013113@gmail.com>
2020-10-12 09:47:15 -0700

    Creation of inner Node Class

--- src/edu/miracosta/cs113/HW4/DoubleLinkedList.java
+++ src/edu/miracosta/cs113/HW4/DoubleLinkedList.java
@@ -37,12 +37,33 @@ public class DoubleLinkedList
 	
 	
 	
+	// Requirement for static inner class Node <E> - taken from text on pg 91 ed.2
+	private static class Node<E>
+	{
+		
+		// Data Fields 
 	
-	
-	
-	
-	
-	
-	
+		// Reference to the data 
+		private E data;
+		
+		// Reference to the next node
+		private Node<E> next;
+		
+		// Constructor - Creates a new node with the next field being null
+		private Node(E dataItem)
+		{
+			data = dataItem;
+			next = null;
+		}
+		
+		// Constructor - Creates a new node that references another node
+		private Node(E dataItem, Node<E> nodeRef)
+		{
+			data = dataItem;
+			next = nodeRef;
+		}	
+		
+	}
+		
 
 }
