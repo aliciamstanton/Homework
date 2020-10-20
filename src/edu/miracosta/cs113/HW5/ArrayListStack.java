ad6146d7f076679ea1e8a408ba8d1c96cc794216
Alicia Stanton <atkelly013113@gmail.com>
2020-10-20 11:45:39 -0700

    Adding an ArrayListStack class

---
+++ src/edu/miracosta/cs113/HW5/ArrayListStack.java
@@ -0,0 +1,29 @@
+package edu.miracosta.cs113.HW5;
+
+public class ArrayListStack implements StackInterface<Integer> {
+
+	@Override
+	public boolean empty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Integer peek() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Integer pop() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Integer push(Integer obj) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
