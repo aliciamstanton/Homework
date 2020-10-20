4be9c4d79123d9533af41ee52a5acb07a848f230
Alicia Stanton <atkelly013113@gmail.com>
2020-10-20 08:43:00 -0700

    Adding Method Stubs and Class for CircularArrayQueue

---
+++ src/edu/miracosta/cs113/HW5/CircularArrayQueue.java
@@ -0,0 +1,45 @@
+package edu.miracosta.cs113.HW5;
+
+import java.util.AbstractQueue;
+import java.util.Iterator;
+import java.util.Queue;
+
+public class CircularArrayQueue<E> extends AbstractQueue<E> implements Queue<E> 
+{
+
+	public CircularArrayQueue(int initialCapacity) 
+	{
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public boolean offer(E e) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public E poll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public E peek() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Iterator<E> iterator() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
