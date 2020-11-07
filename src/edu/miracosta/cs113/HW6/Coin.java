bcad2c06daf019cf63c55f39c6fe3a54136a4c4c
aliciamstanton <atkelly013113@gmail.com>
2020-11-06 20:04:53 -0500

    Creation of coin denominations

--- src/edu/miracosta/cs113/HW6/Coin.java
+++ src/edu/miracosta/cs113/HW6/Coin.java
@@ -6,5 +6,10 @@ package edu.miracosta.cs113.HW6;
 // Class name for Coin Object
 public class Coin 
 {
+	// Instance variables for each coin
+	int quarters, dimes, nickels, pennies;
+	
+	// Full Constructor
+	
 
 }
