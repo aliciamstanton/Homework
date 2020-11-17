3f09bbd254b4ba6d32528bead3f3043d5d0c655f
Alicia Stanton <atkelly013113@gmail.com>
2020-11-17 12:58:31 -0800

    Adding PDF for portion 1 of lab and adding methods for Graph Class

--- src/edu/miracosta/cs113/Lab15/Graph.java
+++ src/edu/miracosta/cs113/Lab15/Graph.java
@@ -7,10 +7,31 @@ import java.util.*;
 public class Graph 
 {
 	// Create a matrix that will hold boolean values 
-	boolean adjacenyMatrix[][];
+	boolean adjacencyMatrix[][];
 	
 	// Int to hold the number of vertices
 	int numberOfVertices;
+	
+	// Constructor
+	public Graph(int numberOfVertices)
+	{
+		this.numberOfVertices = numberOfVertices;
+		adjacencyMatrix = new boolean[numberOfVertices][numberOfVertices];
+	}
+	
+	// Method to add edges to matrix 
+	public void addEdge(int indexOne, int indexTwo)
+	{
+		adjacencyMatrix[indexOne][indexTwo] = true;
+		adjacencyMatrix[indexTwo][indexOne] = true;
+	}
+	
+	// Method to remove edges 
+	public void removeEdge(int indexOne, int indexTwo)
+	{
+		adjacencyMatrix[indexOne][indexTwo] = false;
+		adjacencyMatrix[indexTwo][indexOne] = false;
+	}
 	
 	
 	
