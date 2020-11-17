6d2a7b1bcd1fc895c1ad5ff5bba49f0b094421f5
Alicia Stanton <atkelly013113@gmail.com>
2020-11-17 12:55:00 -0800

    Rename main file

---
+++ src/edu/miracosta/cs113/Lab15/Graph.java
@@ -0,0 +1,20 @@
+
+// Indicates that this can be found in the Lab15 package
+package edu.miracosta.cs113.Lab15;
+
+import java.util.*;
+
+public class Graph 
+{
+	// Create a matrix that will hold boolean values 
+	boolean adjacenyMatrix[][];
+	
+	// Int to hold the number of vertices
+	int numberOfVertices;
+	
+	
+	
+	
+	
+
+}
--- src/edu/miracosta/cs113/Lab15/Main.java
+++
@@ -1,8 +0,0 @@
-
-// Indicates that this can be found in the Lab15 package
-package edu.miracosta.cs113.Lab15;
-
-public class Main 
-{
-
-}
