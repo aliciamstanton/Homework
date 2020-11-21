a59da0a88debef5255505a7d0d5ec04430ce5901
Alicia Stanton <atkelly013113@gmail.com>
2020-11-21 09:16:41 -0800

    Completing Node<E> Class

--- src/edu/miracosta/cs113/HW7/BinaryTree.java
+++ src/edu/miracosta/cs113/HW7/BinaryTree.java
@@ -8,5 +8,31 @@ import java.io.*;
 
 public class BinaryTree<E> implements Serializable 
 {
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	// Inner Node Class - Static 
+	protected static class Node<E> implements Serializable 
+	{
+		// Protected generic data type 
+		protected E data;
+		
+		// Protected Node(s) to represent left and right individually 
+		protected Node<E> left;
+		protected Node<E> right;
+	}
+	
+	
+	
+	
 
 }
