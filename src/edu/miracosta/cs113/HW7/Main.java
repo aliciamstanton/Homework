d7306ec4f24f2cab0eb59cf269123bb99a01074d
Alicia Stanton <atkelly013113@gmail.com>
2020-11-21 17:45:59 -0800

    Creating do-while to drive user input

--- src/edu/miracosta/cs113/HW7/Main.java
+++ src/edu/miracosta/cs113/HW7/Main.java
@@ -5,13 +5,31 @@ package edu.miracosta.cs113.HW7;
 // This functions as the driver for the MorseCode Homework
 public class Main 
 {
+	// Main method 
+	public static void main(String[]args)
+	{
+		
+		
+		
+		
+		// Create the morseCodeTree 
+		MorseCodeTree mct = new MorseCodeTree();
+		
+		
+		// Create int to drive user response
+		int userResponse = 0;
 	
 	
-	// Create the morseCodeTree 
-	MorseCodeTree mct = new MorseCodeTree();
 	
-	// Create a do- while loop to read and execute user input 
-	
+		// Create a do - while loop to read and execute user input 
+		do 
+		{
+		
+		
+		
+		
+		} while(userResponse != 4);
 	
 
+	}
 }
