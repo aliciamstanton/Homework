7765b4bde75e1d94aed85f3d1bd92d041e5e9e05
Alicia Stanton <atkelly013113@gmail.com>
2020-11-21 17:41:55 -0800

    Updating main method

--- src/edu/miracosta/cs113/HW7/Main.java
+++ src/edu/miracosta/cs113/HW7/Main.java
@@ -5,5 +5,13 @@ package edu.miracosta.cs113.HW7;
 // This functions as the driver for the MorseCode Homework
 public class Main 
 {
+	
+	
+	// Create the morseCodeTree 
+	MorseCodeTree mct = new MorseCodeTree();
+	
+	// Create a do- while loop to read and execute user input 
+	
+	
 
 }
