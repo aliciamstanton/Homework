7fdad5a9a9960c33a780dde32cc424e8e9e47e82
Alicia Stanton <atkelly013113@gmail.com>
2020-11-22 17:19:37 -0800

    Moving Inner Entry<K,V>

--- src/edu/miracosta/cs113/HW8/HashTableChain.java
+++ src/edu/miracosta/cs113/HW8/HashTableChain.java
@@ -20,44 +20,6 @@ public class HashTableChain<K, V> implements Map<K,V>
 	
 	
 	
-	// Inner Class Entry - from Text 
-	private static class Entry<K,V> implements Map.Entry<K,V>
-	{
-		
-		// Key and Value
-		private K key;
-		private V value;
-		
-		// Create a new Key Value Pair
-		public Entry(K key, V value)
-		{
-			this.key = key;
-			this.value = value;
-		}
-		
-		
-		// Method to retrieve key
-		public K getKey()
-		{
-			return this.key;
-		}
-		
-		// Method to retrieve value
-		public V getValue()
-		{
-			return this.value;
-		}
-		
-		// Method to set the value
-		public V setValue(V val)
-		{
-			V oldVal = value;
-			value = val;
-			return oldVal;
-		}
-			
-	}
-	
 	
 	
 	
@@ -133,7 +95,44 @@ public class HashTableChain<K, V> implements Map<K,V>
 	}
 	
 	
-	
+	// Inner Class Entry - from Text 
+		private static class Entry<K,V> implements Map.Entry<K,V>
+		{
+			
+			// Key and Value
+			private K key;
+			private V value;
+			
+			// Create a new Key Value Pair
+			public Entry(K key, V value)
+			{
+				this.key = key;
+				this.value = value;
+			}
+			
+			
+			// Method to retrieve key
+			public K getKey()
+			{
+				return this.key;
+			}
+			
+			// Method to retrieve value
+			public V getValue()
+			{
+				return this.value;
+			}
+			
+			// Method to set the value
+			public V setValue(V val)
+			{
+				V oldVal = value;
+				value = val;
+				return oldVal;
+			}
+				
+		}
+		
 	
 	
 	
