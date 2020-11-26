f1cd69f4a4ad0df2ca345118ee8394541d4f68f8
Alicia Stanton <atkelly013113@gmail.com>
2020-11-25 16:46:09 -0800

    Building the translateMorseCode Method

--- src/edu/miracosta/cs113/HW7/MorseCodeTree.java
+++ src/edu/miracosta/cs113/HW7/MorseCodeTree.java
@@ -1,5 +1,10 @@
 package edu.miracosta.cs113.HW7;
 
+// Import 
+import java.util.*;
+import java.io.*;
+
+
 
 /**
  * MorseCodeTree : A BinaryTree, with Nodes of type Character to represent each letter of the English alphabet,
@@ -30,8 +35,39 @@ public class MorseCodeTree extends BinaryTree<Character>
      * @param morseCode The given input representing letters in Morse code
      * @return a String representing the decoded values from morseCode
      */
-    public String translateFromMorseCode(String morseCode) 
+    public String translateFromMorseCode(String encodedMessage) 
     {
+    	
+    	// The goal of this method is to take a string and convert each of its characters 
+    	// into its English equivalent 
+    	
+    	// First make sure that the message only contains values we would find in the tree 
+    	for(int index = 0; index < encodedMessage.length(); index++)
+    	{
+    		if(encodedMessage.charAt(index) == '-')
+    		{
+    			continue;
+    		}
+    		
+    		if(encodedMessage.charAt(index) == '*')
+    		{
+    			continue;
+    		}
+    		
+    		if(encodedMessage.charAt(index) == ' ')
+    		{
+    			continue;
+    		}
+    		
+    		// Otherwise, we cannot process the data and we need to throw an input mismatch exception 
+    		else 
+    		{
+    			throw new InputMismatchException();
+    		}
+    	}
+    	
+    	
+    	
         return "";
     }
     
