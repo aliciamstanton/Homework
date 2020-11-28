9c0c3c96e020ab2ae011cf52c1544dac99a7ba72
Alicia Stanton <atkelly013113@gmail.com>
2020-11-28 11:22:35 -0800

    Completing the AVLNode<E> inner class

--- src/edu/miracosta/cs113/AVLTrees/AVLTree.java
+++ src/edu/miracosta/cs113/AVLTrees/AVLTree.java
@@ -8,4 +8,31 @@ public class AVLTree<E extends Comparable<E>> extends BinarySearchTreeWithRotate
 		
 	
 	// Nested InnerClass for AVLNode<E>
+	private static class AVLNode<E> extends Node<E>
+	{
+		
+		public static final int LEFT_HEAVY = -1;
+		public static final int BALANCED = 0;
+		public static final int RIGHT_HEAVY = 1;
+		
+		private int balance;
+		
+		public AVLNode(E item)
+		{
+			super(item);
+			balance = BALANCED;
+		}
+		
+		@Override
+		public String toString()
+		{
+			return balance + ": " + super.toString();
+		}
+		
+		
+		
+		
+		
+		
+	}
 }
