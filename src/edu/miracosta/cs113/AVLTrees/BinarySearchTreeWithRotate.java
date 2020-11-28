d1867b02f76c0af1c60b3066cf7be8020fcbf0a4
Alicia Stanton <atkelly013113@gmail.com>
2020-11-28 11:13:07 -0800

    Providing Method header

--- src/edu/miracosta/cs113/AVLTrees/BinarySearchTreeWithRotate.java
+++ src/edu/miracosta/cs113/AVLTrees/BinarySearchTreeWithRotate.java
@@ -1,6 +1,6 @@
 package edu.miracosta.cs113.AVLTrees;
 
-public class BinarySearchTreeWithRotate 
+public class BinarySearchTreeWithRotate<E extends Comparable<E>> extends BinarySearchTree<E> 
 {
 
 }
