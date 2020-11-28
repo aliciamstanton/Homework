d8b7a6bc4f75fcf988d7d572b3719d84a321a3c2
Alicia Stanton <atkelly013113@gmail.com>
2020-11-28 11:14:25 -0800

    Creating method for rotateRight

--- src/edu/miracosta/cs113/AVLTrees/BinarySearchTreeWithRotate.java
+++ src/edu/miracosta/cs113/AVLTrees/BinarySearchTreeWithRotate.java
@@ -2,5 +2,13 @@ package edu.miracosta.cs113.AVLTrees;
 
 public class BinarySearchTreeWithRotate<E extends Comparable<E>> extends BinarySearchTree<E> 
 {
+	
+	protected Node<E> rotateRight(Node<E> root)
+	{
+		Node<E> temp = root.left;
+		root.left = temp.right;
+		temp.right = root;
+		return temp;
+	}
 
 }
