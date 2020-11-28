fc88ec4c5b3003a5cf220fe3522a978638cdf414
Alicia Stanton <atkelly013113@gmail.com>
2020-11-28 11:20:04 -0800

    Updating for AVLNode<E> Class

--- src/edu/miracosta/cs113/AVLTrees/AVLTree.java
+++ src/edu/miracosta/cs113/AVLTrees/AVLTree.java
@@ -2,5 +2,10 @@ package edu.miracosta.cs113.AVLTrees;
 
 public class AVLTree<E extends Comparable<E>> extends BinarySearchTreeWithRotate<E> 
 {
-
+	private boolean increase;
+	
+	
+		
+	
+	// Nested InnerClass for AVLNode<E>
 }
