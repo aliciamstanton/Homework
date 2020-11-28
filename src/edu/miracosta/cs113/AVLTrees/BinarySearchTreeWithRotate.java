49685e3a6853463112dece92f24d2619677d99f4
Alicia Stanton <atkelly013113@gmail.com>
2020-11-28 11:15:56 -0800

    Writing method for rotateLeft in BinarySearchTreeWithRotate

--- src/edu/miracosta/cs113/AVLTrees/BinarySearchTreeWithRotate.java
+++ src/edu/miracosta/cs113/AVLTrees/BinarySearchTreeWithRotate.java
@@ -3,6 +3,7 @@ package edu.miracosta.cs113.AVLTrees;
 public class BinarySearchTreeWithRotate<E extends Comparable<E>> extends BinarySearchTree<E> 
 {
 	
+	// Rotate Right 
 	protected Node<E> rotateRight(Node<E> root)
 	{
 		Node<E> temp = root.left;
@@ -10,5 +11,16 @@ public class BinarySearchTreeWithRotate<E extends Comparable<E>> extends BinaryS
 		temp.right = root;
 		return temp;
 	}
+	
+	// Rotate Left 
+	protected Node<E> rotateLeft(Node<E> root)
+	{
+		Node<E> temp = root.right;
+		root.right = temp.left;
+		temp.left = root;
+		return temp;
+	}
+	
+	
 
 }
