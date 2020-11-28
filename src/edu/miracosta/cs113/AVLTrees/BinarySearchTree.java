dfb804bcfe44b2aeaa7048173e39d475d0d9bf7c
Alicia Stanton <atkelly013113@gmail.com>
2020-11-27 20:33:54 -0800

    Creating class header for BinarySearchTree

--- src/edu/miracosta/cs113/AVLTrees/BinarySearchTree.java
+++ src/edu/miracosta/cs113/AVLTrees/BinarySearchTree.java
@@ -3,7 +3,7 @@ package edu.miracosta.cs113.AVLTrees;
 
 
 // Implementation of the BinarySearchTree Class 
-public class BinarySearchTree
+public class BinarySearchTree< E extends Comparable<E>> extends BinaryTree<E> implements SearchTree<E>
 {
 
 }
