ba166d8a2acab968bff0bd81b8bebfd27a6bb9a0
Alicia Stanton <atkelly013113@gmail.com>
2020-11-28 11:57:13 -0800

    Finishing the recursive add method in AVL

--- src/edu/miracosta/cs113/AVLTrees/AVLTree.java
+++ src/edu/miracosta/cs113/AVLTrees/AVLTree.java
@@ -5,6 +5,83 @@ public class AVLTree<E extends Comparable<E>> extends BinarySearchTreeWithRotate
 	private boolean increase;
 	
 	
+	@Override
+	public boolean add(E item)
+	{
+		increase = false;
+		root = add((AVLNode<E>) root, item);
+		return addReturn;
+	}
+	
+	// Recursive add method 
+	private AVLNode<E> add(AVLNode<E> localRoot, E item)
+	{
+		if(localRoot == null)
+		{
+			addReturn = true;
+			increase = true;
+			return new AVLNode<E>(item);
+		}
+		
+		if(item.compareTo(localRoot.data)== 0) 
+		{
+			increase = false;
+			addReturn = false;
+			return localRoot;
+		}
+		
+		else if(item.compareTo(localRoot.data) < 0)
+		{
+			localRoot.left = add((AVLNode<E>) localRoot.left, item);
+			
+			if(increase)
+			{
+				decrementBalance(localRoot);
+				if(localRoot.balance < AVLNode.LEFT_HEAVY) 
+				{
+					increase = false;
+					return rebalanceLeft(localRoot);
+					
+				}
+			}
+		}
+		
+		else if(item.compareTo(localRoot.data) > 0)
+		{
+			localRoot.right = add((AVLNode<E>) localRoot.right, item);
+			
+			if(increase)
+			{
+				incrementBalance(localRoot);
+				if(localRoot.balance > AVLNode.RIGHT_HEAVY)
+				{
+					increase = false;
+					return rebalanceRight(localRoot);
+				}
+			}
+		}
+		
+		return localRoot;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 		
 	
 	// Nested InnerClass for AVLNode<E>
