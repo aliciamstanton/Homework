2819b903c852223c2818aa74daa5bb6720924fc5
Alicia Stanton <atkelly013113@gmail.com>
2020-12-05 09:58:09 -0800

    Commenting driver program for AVL Tree

--- src/edu/miracosta/cs113/AVLTrees/Main.java
+++ src/edu/miracosta/cs113/AVLTrees/Main.java
@@ -1,15 +1,21 @@
 package edu.miracosta.cs113.AVLTrees;
 
-import java.util.Random;
 
+// Import statements to ensure that we can generate random numbers
+// to place in the BinarySearchTree and the AVLTree
+import java.util.Random;
 import java.util.*;
+
+
+
+
 // Driver program for the AVL Tree 
 public class Main 
 {
 	public static void main(String[] args)
 	{
 	
-	// Create a binarySearchTree 
+	// Create a binarySearchTree - integer values 
 	BinarySearchTree<Integer> newSearchTree = new BinarySearchTree<Integer>();
 	
 	// Create an AVL Tree of Integers
@@ -19,13 +25,15 @@ public class Main
 	Random randomNumber = new Random();
 	
 	// Create an array of integers to hold the random numbers we've generated 
+	// This array holds 20 random numbers 
 	int[] randomValues = new int[20];
 	
 	
 	// Populate the array with 20 random numbers between 0 and 499
 	for(int i = 0; i < randomValues.length; i++)
 	{
-		
+		// Choose the next int and put it at the next available index in the 
+		// random values array 
 		randomValues[i] = randomNumber.nextInt(500);
 		
 	}
@@ -33,12 +41,14 @@ public class Main
 	// Add these items to the search tree
 	for(int i = 0; i < randomValues.length; i++)
 	{
+		// Call the add method and put this in a new node of the binarySearchTree 
 		newSearchTree.add(randomValues[i]);
 	}
 	
 	// Add the items to the AVL Tree
 	for(int i = 0; i < randomValues.length; i++)
 	{
+		// Call the add method and put this in a new node of the AVLTree 
 		newAVLTree.add(randomValues[i]);
 	}
 	
