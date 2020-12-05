a753f47187838db5635534c8c2d49831f5af8c8d
Alicia Stanton <atkelly013113@gmail.com>
2020-12-05 10:12:37 -0800

    Updating

--- src/edu/miracosta/cs113/AVLTrees/BinarySearchTree.java
+++ src/edu/miracosta/cs113/AVLTrees/BinarySearchTree.java
@@ -4,6 +4,8 @@ package edu.miracosta.cs113.AVLTrees;
 
 // Implementation of the BinarySearchTree Class 
 @SuppressWarnings("Serial")
+
+
 public class BinarySearchTree< E extends Comparable<E>> extends BinaryTree<E> implements SearchTree<E>
 {
 	protected boolean addReturn;
